e311c0f05095ca1ade4c7aad5ea51785c85cb52c
adamjgoodman <goodmanadamj@gmail.com>
2019-12-26 03:38:23 +0000

    added STI for Piece model, pawn, rook, bishop, knight, king, queen models

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,28 @@
 FactoryBot.define do
+  factory :king do
+    
+  end
+
+  factory :queen do
+    
+  end
+
+  factory :bishop do
+    
+  end
+
+  factory :knight do
+    
+  end
+
+  factory :rook do
+    
+  end
+
+  factory :pawn do
+    
+  end
+
   factory :user do
     sequence :email do |n|
       "dummyEmail#{n}@gmail.com"
