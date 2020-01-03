03b0473f58306a8a16b93ef7c04ec98652b94c0a
Simon Fuchs <sfuchs42@gmail.com>
2020-01-03 15:48:08 +0000

    Refactored obstructed/added king.rb

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,117 +1,87 @@
 class Piece < ApplicationRecord
 
-  def horz?(piece, goal_x, goal_y)
-    if piece.y_pos == goal_y && piece.x_pos != goal_x
+  def horz?(goal_x, goal_y)
+    y_pos == goal_y && x_pos != goal_x
+  end
+  
+  def vert?(goal_x, goal_y)
+    y_pos != goal_y && x_pos == goal_x
+  end
+  
+  def diag?(goal_x, goal_y)
+    (goal_y - y_pos).abs == (goal_x - x_pos).abs
+  end
+  
+  def invalid?(goal_x, goal_y)
+    dist_y = (goal_y - y_pos).abs
+    dist_x = (goal_x - x_pos).abs
+    if dist_x == 0 || dist_y == 0 
+      return false
+    elsif dist_x != dist_y
       return true
     end
     return false
   end
   
-  def vert?(piece, goal_x, goal_y)
-    if piece.y_pos != goal_y && piece.x_pos == goal_x
-      return true
-    end
-    return false
-  end
-  
-  def diag?(piece, goal_x, goal_y)
-    if piece.y_pos != goal_y && piece.x_pos != goal_x
-      return true
-    end
-    return false
-  end
-  
-  def invalid?(piece, goal_x, goal_y)
-    dist_y = (goal_y - piece.y_pos).abs
-    dist_x = (goal_x - piece.x_pos).abs
-    if dist_x != dist_y
-      return true
-    end
-    return false
-  end
-  
-  def hor(piece, goal)
-    dist = (goal - piece.x_pos).abs
+  def hor(goal_x, goal_y)
+    dist = (goal_x - x_pos).abs
     dist.times do |i|
-      if goal > piece.x_pos
-        if board[piece.y_pos][piece.x_pos + i] != 0
-          return 'is obstructed'
-        end
+      if goal_x > x_pos
+        return true if Piece.where(x_pos: x_pos + i, y_pos: goal_y, game_id: game_id).count != 0 
       else
-        if board[piece.y_pos][piece.x_pos - i] != 0
-          return 'is obstructed'
-        end
-      end
-    end 
-  end
-  
-  def vert(piece, goal)
-    dist = (goal - piece.y_pos).abs
-    dist.times do |i|
-      if goal > piece.y_pos
-        if board[piece.y_pos - i][piece.x_pos] != 0
-          return 'is obstructed'
-        end
-      else
-        if board[piece.y_pos + i][piece.x_pos] != 0
-          return 'is obstructed'
-        end
+        return true if Piece.where(x_pos: x_pos - i, y_pos: goal_y, game_id: game_id).count != 0 
       end
     end
+    false 
+  end
+  
+  def vert(goal_x, goal_y)
+    dist = (goal_y - y_pos).abs
+    dist.times do |i|
+      if goal_y > y_pos
+        return true if Piece.where(x_pos: goal_x, y_pos: y_pos + 1, game_id: game_id).count != 0
+      else
+        return true if Piece.where(x_pos: goal_x, y_pos: y_pos - 1, game_id: game_id).count != 0
+      end
+    end
+    false
   end
     
-  def diag (piece, goal_x, goal_y)
-    dist_y = goal_y - piece.y_pos
-    dist_x = goal_x - piece.x_pos
+  def diag (goal_x, goal_y)
+    dist_y = goal_y - y_pos
+    dist_x = goal_x - x_pos
     dist_x.abs.times do |i|
       if dist_x < 0 && dist_y < 0
-        if board[piece.y_pos - i][piece.x_pos - i] != 0
-          return 'is obstructed'
-        end
+        return true if Piece.where(x_pos: x_pos - i, y_pos: y_pos - i, game_id: game_id).count != 0
       elsif dist_x < 0 && dist_y > 0
-        if board[piece.y_pos + i][piece.x_pos - i] != 0
-          return 'is obstructed'
-        end
+        return true if Piece.where(x_pos: x_pos - i, y_pos: y_pos + i, game_id: game_id).count != 0
       elsif dist_x > 0 && dist_y < 0
-        if board[piece.y_pos - i][piece.x_pos + i] != 0
-          return 'is obstructed'
-        end
+        return true if Piece.where(x_pos: x_pos + i, y_pos: y_pos - i, game_id: game_id).count != 0
       else
-        if board[piece.y_pos + i][piece.x_pos + i] != 0
-          return 'is obstructed'
-        end
+        return true if Piece.where(x_pos: x_pos + i, y_pos: y_pos + i, game_id: game_id).count != 0
       end
     end
+    false
   end
 
-  def obstructed?(piece, goal_x, goal_y)
-    if invalid?(piece, goal_x, goal_y)
-      return "Invalid choice"
-    elsif horz?(piece, goal_x, goal_y)
-      return hor(piece, goal_x)
-    elsif vert?(piece, goal_x, goal_y)
-      return vert(piece, goal_y)
-    else
-      return diag(piece, goal_x, goal_y)
-    end
-  end
-end
-
-class King < Piece
-
-  def initialize(piece)
-    @King = piece
+  def obstructed?(goal_x, goal_y)
+    raise "Invalid choice. Please choose again" unless horz?(goal_x, goal_y) || vert?(goal_x, goal_y) || diag?(goal_x, goal_y)
+    
+    return hor(goal_x, goal_y) if horz?(goal_x, goal_y)
+      
+    return vert(goal_x, goal_y) if vert?(goal_x, goal_y)
+      
+    return diag(goal_x, goal_y) if diag?(goal_x, goal_y)
+    
   end
 
   def move(goal_x, goal_y)
-    if @king.obstructed? != "Invalid choice" && @king.obstructed? != "is obstructed"
-      x_dist = (goal_x - @king.x_pos).abs
-      y_dist = (goal_y - @king.y_pos).abs
-      if x_dist == 1 || y_dist == 1
-        @king.x_pos = goal_x
-        @king.y_pos = goal_y
-      end
+    if move? 
+      x_pos = goal_x
+      y_pos = goal_y
+      save!
+    else
+      raise 'Invalid move. Try again: '
     end
   end
-
 end
--- spec/is_obstructed_spec.rb
+++ spec/is_obstructed_spec.rb
@@ -6,25 +6,34 @@ RSpec.describe Piece, type: :model do
   # !HORZ?
   it 'takes in desired destination(goal) and compares to piece current position to check if HORIZONTAL' do
     piece1 = FactoryBot.create(:piece)
-    expect(piece1.horz?(piece1, 7, 4)).to eq(true)
-    expect(piece1.horz?(piece1, 4, 7)).to eq(false)
+    expect(piece1.horz?(7, 4)).to eq(true)
+    expect(piece1.horz?(4, 7)).to eq(false)
   end
   # !VERT?
   it 'takes in desired destination(goal) and compares to piece current position to check if VERTICAL' do
     piece1 = FactoryBot.create(:piece)
-    expect(piece1.vert?(piece1, 7, 4)).to eq(false)
-    expect(piece1.vert?(piece1, 4, 7)).to eq(true)
+    expect(piece1.vert?(7, 4)).to eq(false)
+    expect(piece1.vert?(4, 7)).to eq(true)
   end
   # !DIAG?
   it 'takes in desired destination(goal) and compares to piece current position to check if DIAGONIAL' do
     piece1 = FactoryBot.create(:piece)
-    expect(piece1.diag?(piece1, 7, 4)).to eq(false)
-    expect(piece1.diag?(piece1, 5, 7)).to eq(true)
+    expect(piece1.diag?(7, 4)).to eq(false)
+    expect(piece1.diag?(3, 5)).to eq(true)
+    expect(piece1.diag?(5, 5)).to eq(true)
   end
   # !INVALID?
   it 'takes in desired destination(goal) and compares to piece current position to check if INVALID' do
     piece1 = FactoryBot.create(:piece)
-    expect(piece1.invalid?(piece1, 1, 5)).to eq(true)
-    expect(piece1.invalid?(piece1, 6, 6)).to eq(false)
+    expect(piece1.invalid?(1, 5)).to eq(true)
+    expect(piece1.invalid?(6, 6)).to eq(false)
+  end
+  # !OBSTRUCTED?
+  it 'takes in desired destination(goal) and compares to piece current position to check if all of the above' do
+    piece1 = FactoryBot.create(:piece)
+    expect(piece1.obstructed?(4, 7)).to eq(false)
+    expect(piece1.obstructed?(7, 4)).to eq(true)
+    expect(piece1.obstructed?(3, 5)).to eq(true)
+    expect(piece1.obstructed?(3, 7)).to raise_error(RuntimeError)
   end
 end
