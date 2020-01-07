1f991323fd894bf3765258c4192714037e81e064
James <jguest777@gmail.com>
2020-01-07 19:02:39 +0000

    added white_user_name to gameModel

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,8 @@ class Game < ApplicationRecord
   scope :available, -> { where(black_id: nil) }
   belongs_to :user, required: false
   has_many :pieces
+
+  def white_user_name
+    User.find_by(:id white_id).user_id
+  end
 end
