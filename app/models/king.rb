638b7ae028cde050af212e60b38abababb1cd089
Simon Fuchs <sfuchs42@gmail.com>
2020-01-08 17:44:44 +0000

    Cleaned up tests and added comments

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,5 +1,5 @@
 class King < Piece
-
+  # the king can move in any direction, but only one space at a time
   def move?(goal_x, goal_y)
     return (goal_x - self.x_pos).abs <= 1 && (goal_y - self.y_pos).abs <= 1 unless obstructed?(goal_x, goal_y)
     false
