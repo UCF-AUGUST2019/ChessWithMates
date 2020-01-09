9a2388cc6b6b3c7df0b071feeafe733becf9abde
kimberly sanchez <kxmbers22@gmail.com>
2020-01-09 01:55:18 +0000

    include devide in rails helper and update games_controllers spec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -4,10 +4,10 @@ require 'rspec/rails'
 RSpec.describe GamesController, type: :controller do
 	
 	describe 'game#show action' do
-		it 'should successfully show the gameboard' do
+		it 'should require sign in to show the gameboard' do
 			game = FactoryBot.create(:game)
 			get :show, params: { id: game.id }
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 end
