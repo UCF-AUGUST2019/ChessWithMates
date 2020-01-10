d4b39e49f5fff74848f7f3b53ab67febc75b7b11
James <jguest777@gmail.com>
2020-01-10 23:00:32 +0000

    Queen move logic working and tests passing

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Queen, type: :model do
-  it 'checks if desired move is horz, vert or diag' do
-    user = FactoryBot.create(:user)
-    game = FactoryBot.create(:game)
-    FactoryBot.create(:piece, game: game, player_id: user.id, type: 'King')
-    piece = Piece.where(game: game, player_id: user.id, type: 'Queen').first
-    expect(piece.move(4, 7)).to eq(true)
-    expect(piece.move(7, 4)).to eq(true)
-    expect(piece.move(1, 7)).to eq(true)
-    expect(piece.move(7, 7)).to eq(true)
-    expect(piece.move(6, 3)).to eq(false)
-    expect(piece.move(3, 6)).to eq(false)
+   it 'checks if desired move is horz, vert or diag' do
+    user1 = FactoryBot.create(:user)
+    game1 = FactoryBot.create(:game)
+    FactoryBot.create(:piece, game: game1, player_id: user1.id, type: 'Queen')
+
+    piece1 = Piece.where(game: game1, player_id: user1.id, type: 'Queen').first
+    expect(piece1.move(4, 7)).to eq(true)
+    expect(piece1.move(7, 4)).to eq(true)
+    expect(piece1.move(1, 7)).to eq(true)
+    expect(piece1.move(7, 7)).to eq(true)
+    expect(piece1.move(6, 3)).to eq('Invalid move. Try again: ')
+    expect(piece1.move(3, 6)).to eq('Invalid move. Try again: ')
   end
 end
