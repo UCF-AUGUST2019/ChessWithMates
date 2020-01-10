b777b809aeaa1d264e5c66b79ccb3f4778c3da2b
James <jguest777@gmail.com>
2020-01-10 22:52:04 +0000

    adding Queen move logic with tests

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Queen, type: :model do
+  it 'checks if desired move is horz, vert or diag' do
+    user = FactoryBot.create(:user)
+    game = FactoryBot.create(:game)
+    FactoryBot.create(:piece, game: game, player_id: user.id, type: 'King')
+    piece = Piece.where(game: game, player_id: user.id, type: 'Queen').first
+    expect(piece.move(4, 7)).to eq(true)
+    expect(piece.move(7, 4)).to eq(true)
+    expect(piece.move(1, 7)).to eq(true)
+    expect(piece.move(7, 7)).to eq(true)
+    expect(piece.move(6, 3)).to eq(false)
+    expect(piece.move(3, 6)).to eq(false)
+  end
 end
