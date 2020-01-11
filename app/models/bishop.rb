c6a13a3f3948e33feab16f662382aa8af99374e3
kimberly sanchez <kxmbers22@gmail.com>
2020-01-11 20:26:03 +0000

    bishop move

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -4,4 +4,9 @@ class Bishop < Piece
   	'fa fa-chess-bishop'
   end
 
+  def move?(goal_x, goal_y)
+    return (goal_x - x_pos).abs == (goal_y - y_pos).abs unless obstructed?(goal_x, goal_y)
+    false
+  end
+
 end
