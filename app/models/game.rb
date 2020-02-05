ffd29d667911d7adeeea39e5cd7c5b3e31c1cd86
James <jguest777@gmail.com>
2020-02-05 22:22:18 +0000

    updated available_games scope to set game_over to false instead of nil

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  scope :available, -> { where(black_id: nil, game_over: nil) }
+  scope :available, -> { where(black_id: nil, game_over: false) }
   belongs_to :user, required: false
   belongs_to :white_player, class_name: 'User', foreign_key: 'white_id', optional: true
   belongs_to :black_player, class_name: 'User', foreign_key: 'black_id', optional: true
